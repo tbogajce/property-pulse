7178f7586008218b980cfaf65e06ab147685b698
Tihomir Bogajcevic <tihomir.bogajcevic@symphony.is>
2024-11-14 12:00:06 +0100

    Database models

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: "string",
+      unique: [true, "Email already exists"],
+      required: [true, "Email is required"],
+    },
+    username: {
+      type: "string",
+      required: [true, "Username is required"],
+    },
+    image: {
+      type: "string",
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = models.user || model("User", UserSchema);
+
+export default User;
