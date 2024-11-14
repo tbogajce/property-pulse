9b980133695bfda52efa1ce7473228ca2867eff5
Tihomir Bogajcevic <tihomir.bogajcevic@symphony.is>
2024-11-14 16:48:00 +0100

    OAuth and SessionProvider

---
+++ utils/authOptions.js
@@ -0,0 +1,23 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // invoked on successful signin
+    async signIn({ profile }) {},
+    // modifies the session
+    async session({ session }) {},
+  },
+};
