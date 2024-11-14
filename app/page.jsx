144711baa294f3f8a10b74ba3da52671da4e1da0
Tihomir Bogajcevic <tihomir.bogajcevic@symphony.is>
2024-11-14 11:26:02 +0100

    Loading and NotFound

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI);
   return (
     <>
       <Hero />
