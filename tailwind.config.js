c51ed4d12036343b1f0668282b9d0dca56bacd6c
Tihomir Bogajcevic <tihomir.bogajcevic@symphony.is>
2024-11-14 14:50:08 +0100

    Property page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
     },
   },
   plugins: [],
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all properties
 export const fetchProperties = async () => {
   try {
     // handle if the domain is not available yet
@@ -13,5 +14,21 @@ export const fetchProperties = async () => {
   } catch (error) {
     console.log(error);
     return [];
+  }
+};
+
+// fetch single property
+export const fetchProperty = async (id) => {
+  try {
+    if (!apiDomain) return null;
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res) throw new Error("Failed to fetch a property");
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 };
